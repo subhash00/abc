1306466398bf398beb8759c635aac8ff8003e7d4
subhash00 <subhahpathak1991@gmail.com>
2020-09-11 23:07:49 +0530

    Updates

---
+++ precache-manifest.95fae29c71be076657aa492cb19b8b56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5261a6cc838bf395c8627daff6f4482e",
+    "url": "/simplecalculator/index.html"
+  },
+  {
+    "revision": "f4dd84fab63101451e78",
+    "url": "/simplecalculator/static/css/main.d6e1251f.chunk.css"
+  },
+  {
+    "revision": "438e879ea899b881512c",
+    "url": "/simplecalculator/static/js/2.305387d7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simplecalculator/static/js/2.305387d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4dd84fab63101451e78",
+    "url": "/simplecalculator/static/js/main.f373bfd4.chunk.js"
+  },
+  {
+    "revision": "d0ffac4210f90f6cee8b",
+    "url": "/simplecalculator/static/js/runtime-main.7024123c.js"
+  }
+]);
